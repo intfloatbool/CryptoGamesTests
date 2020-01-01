81f8686ac18b0e0ffea21358e1a74427beffa171
intfloatbool <intfloatbool@gmail.com>
2020-01-01 17:06:01 +0300

    CardPlayer test

--- Games/Poker/HoldemGameProecessTests.cs
+++ Games/Poker/HoldemGameProecessTests.cs
@@ -1,4 +1,5 @@
-﻿using EthWebPoker.Games.CardGames.CardBase;
+﻿using EthWebPoker.Games.CardGames;
+using EthWebPoker.Games.CardGames.CardBase;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -37,6 +38,28 @@ namespace CryptoGamesTests.Games.Poker
             Assert.AreEqual(ranksCount, heartsCards.Count());
             Assert.AreEqual(ranksCount, diamondsCards.Count());
             Assert.AreEqual(ranksCount, clubsCards.Count());
+        }
+
+        [TestMethod]
+        public void CardPlayerTest()
+        {
+            var player1 = new CardPlayer();
+            var player2 = new CardPlayer();
+
+            var card1 = new Card(Rank.ACE, Suit.HEARTS);
+            var card2 = new Card(Rank.TWO, Suit.SPADES);
+
+            player1.AddCard(card1);
+            player1.AddCard(card2);
+
+            player2.AddCard(new Card(Rank.SEVEN, Suit.CLUBS));
+            player2.AddCard(new Card(Rank.NINE, Suit.DIAMONDS));
+            player2.FoldCards();
+
+            Assert.IsTrue(player1.Cards.Contains(card1));
+            Assert.IsTrue(player1.Cards.Contains(card2));
+            Assert.IsFalse(player2.Cards.Any());
+
         }
     }
 }
