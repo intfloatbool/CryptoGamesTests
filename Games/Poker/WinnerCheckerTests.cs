720583098f46ac7730e824ebb37048acb2afe261
intfloatbool <intfloatbool@gmail.com>
2020-01-06 09:55:41 +0300

    FlushRoyal win checking

--- Games/Poker/WinnerCheckerTests.cs
+++ Games/Poker/WinnerCheckerTests.cs
@@ -654,6 +654,109 @@ namespace CryptoGamesTests.Games.Poker
 
         //Afterwards we will skip all tests with "same combo", 
         //becouse there is cannot be same combo in next combinations!
-        
+        [TestMethod]
+        public void OnePlayerWithFourOfAKind()
+        {
+            var p1 = new CardPlayer();
+            var p2 = new CardPlayer();
+            var tableCards = new CardTable();
+            var winnerChecker = new WinnerChecker();
+
+            //P1 with FullHouse: 4-4-4-9-9
+            //P2 with Four of a Kind : 9-9-9-9
+            p1.AddCard(new Card(Rank.FOUR, Suit.CLUBS));
+            p1.AddCard(new Card(Rank.FOUR, Suit.DIAMONDS));
+
+            p2.AddCard(new Card(Rank.NINE, Suit.SPADES));
+            p2.AddCard(new Card(Rank.NINE, Suit.DIAMONDS));
+
+            tableCards.AddCard(new Card(Rank.FOUR, Suit.SPADES));
+            tableCards.AddCard(new Card(Rank.NINE, Suit.HEARTS));
+            tableCards.AddCard(new Card(Rank.NINE, Suit.CLUBS));
+            tableCards.AddCard(new Card(Rank.SIX, Suit.SPADES));
+            tableCards.AddCard(new Card(Rank.TWO, Suit.DIAMONDS));
+
+            var winnerContainer = winnerChecker.GetWinnerWithCombo(new[]
+            {
+                p1, p2
+            }, tableCards);
+
+            Assert.IsNotNull(winnerContainer);
+            Assert.IsNotNull(winnerContainer.Players);
+            Assert.IsNotNull(winnerContainer.Combination);
+            Assert.AreEqual(1, winnerContainer.Players.Count);
+            Assert.IsTrue(winnerContainer.Players.Contains(p2));
+            Assert.AreEqual(Combo.FOUR_OF_A_KIND, winnerContainer.Combination);
+        }
+    
+        [TestMethod]
+        public void OnePlayerWithStraigthFlush()
+        {
+            var p1 = new CardPlayer();
+            var p2 = new CardPlayer();
+            var tableCards = new CardTable();
+            var winnerChecker = new WinnerChecker();
+
+            //P1 with straigth-flush: 7-8-9-T-J of Spades
+            //P2 with three of kind: J-J-J
+            p1.AddCard(new Card(Rank.SEVEN, Suit.SPADES));
+            p1.AddCard(new Card(Rank.EIGHT, Suit.SPADES));
+
+            p2.AddCard(new Card(Rank.JACK, Suit.HEARTS));
+            p2.AddCard(new Card(Rank.JACK, Suit.DIAMONDS));
+
+            tableCards.AddCard(new Card(Rank.JACK, Suit.SPADES));
+            tableCards.AddCard(new Card(Rank.ACE, Suit.HEARTS));
+            tableCards.AddCard(new Card(Rank.NINE, Suit.SPADES));
+            tableCards.AddCard(new Card(Rank.TEN, Suit.SPADES));
+            tableCards.AddCard(new Card(Rank.TWO, Suit.DIAMONDS));
+
+            var winnerContainer = winnerChecker.GetWinnerWithCombo(new[]
+            {
+                p1, p2
+            }, tableCards);
+
+            Assert.IsNotNull(winnerContainer);
+            Assert.IsNotNull(winnerContainer.Players);
+            Assert.IsNotNull(winnerContainer.Combination);
+            Assert.AreEqual(1, winnerContainer.Players.Count);
+            Assert.IsTrue(winnerContainer.Players.Contains(p1));
+            Assert.AreEqual(Combo.STRAIGHT_FLUSH, winnerContainer.Combination);
+        }
+
+        [TestMethod]
+        public void OnePlayerWithRoyalFlush()
+        {
+            var p1 = new CardPlayer();
+            var p2 = new CardPlayer();
+            var tableCards = new CardTable();
+            var winnerChecker = new WinnerChecker();
+
+            //P2 with four of kind: J-J-J-J
+            //P2 with royal-flush: T-J-Q-K-A of Diamonds
+            p1.AddCard(new Card(Rank.JACK, Suit.HEARTS));
+            p1.AddCard(new Card(Rank.JACK, Suit.SPADES));
+
+            p2.AddCard(new Card(Rank.ACE, Suit.DIAMONDS));
+            p2.AddCard(new Card(Rank.KING, Suit.DIAMONDS));
+
+            tableCards.AddCard(new Card(Rank.JACK, Suit.DIAMONDS));
+            tableCards.AddCard(new Card(Rank.QUEEN, Suit.DIAMONDS));
+            tableCards.AddCard(new Card(Rank.JACK, Suit.CLUBS));
+            tableCards.AddCard(new Card(Rank.TEN, Suit.DIAMONDS));
+            tableCards.AddCard(new Card(Rank.TWO, Suit.CLUBS));
+
+            var winnerContainer = winnerChecker.GetWinnerWithCombo(new[]
+            {
+                p1, p2
+            }, tableCards);
+
+            Assert.IsNotNull(winnerContainer);
+            Assert.IsNotNull(winnerContainer.Players);
+            Assert.IsNotNull(winnerContainer.Combination);
+            Assert.AreEqual(1, winnerContainer.Players.Count);
+            Assert.IsTrue(winnerContainer.Players.Contains(p2));
+            Assert.AreEqual(Combo.ROYAL_FLUSH, winnerContainer.Combination);
+        }
     }
 }
