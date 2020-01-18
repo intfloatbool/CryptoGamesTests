04ded785b91b8d196ea97ac30bcf30f9cb7cbf51
intfloatbool <intfloatbool@gmail.com>
2020-01-18 14:12:45 +0300

    Dice game process result

--- Games/Dices/DiceTests.cs
+++ Games/Dices/DiceTests.cs
@@ -1,7 +1,9 @@
-﻿using EthWebPoker.Games.Dices;
+﻿using EthWebPoker.Games;
+using EthWebPoker.Games.Dices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CryptoGamesTests.Games.Dices
@@ -40,6 +42,61 @@ namespace CryptoGamesTests.Games.Dices
             Assert.IsTrue(winningValuesOfSecondDice.Contains(4));
             Assert.IsTrue(winningValuesOfSecondDice.Contains(5));
             Assert.IsTrue(winningValuesOfSecondDice.Contains(6));
+        }
+
+        [TestMethod]
+        public void DiceGameResultPlayer1_Win()
+        {
+            var diceGame = new DiceGame();
+            var player1 = diceGame.GetPlayerByType(PlayerType.PLAYER_1);
+            var player2 = diceGame.GetPlayerByType(PlayerType.PLAYER_2);
+
+            player1.DiceCubes.ExternalResult = new DiceCubes.DiceResult(6, 4);
+            player2.DiceCubes.ExternalResult = new DiceCubes.DiceResult(3, 1);
+            diceGame.Start();
+
+            var winners = diceGame.Winners.ToList();
+            Assert.AreEqual(10, player1.GetSum());
+            Assert.AreEqual(4, player2.GetSum());
+            Assert.AreEqual(1, winners.Count);
+            Assert.IsTrue(winners.Contains(player1));
+        }
+
+        [TestMethod]
+        public void DiceGameResultPlayer2_Win()
+        {
+            var diceGame = new DiceGame();
+            var player1 = diceGame.GetPlayerByType(PlayerType.PLAYER_1);
+            var player2 = diceGame.GetPlayerByType(PlayerType.PLAYER_2);
+
+            player1.DiceCubes.ExternalResult = new DiceCubes.DiceResult(2, 1);
+            player2.DiceCubes.ExternalResult = new DiceCubes.DiceResult(4, 5);
+            diceGame.Start();
+
+            var winners = diceGame.Winners.ToList();
+            Assert.AreEqual(3, player1.GetSum());
+            Assert.AreEqual(9, player2.GetSum());
+            Assert.AreEqual(1, winners.Count);
+            Assert.IsTrue(winners.Contains(player2));
+        }
+
+        [TestMethod]
+        public void DiceGameResultBothPlayers_Win()
+        {
+            var diceGame = new DiceGame();
+            var player1 = diceGame.GetPlayerByType(PlayerType.PLAYER_1);
+            var player2 = diceGame.GetPlayerByType(PlayerType.PLAYER_2);
+
+            player1.DiceCubes.ExternalResult = new DiceCubes.DiceResult(4, 4);
+            player2.DiceCubes.ExternalResult = new DiceCubes.DiceResult(3, 5);
+            diceGame.Start();
+
+            var winners = diceGame.Winners.ToList();
+            Assert.AreEqual(8, player1.GetSum());
+            Assert.AreEqual(8, player2.GetSum());
+            Assert.AreEqual(2, winners.Count);
+            Assert.IsTrue(winners.Contains(player1));
+            Assert.IsTrue(winners.Contains(player2));
         }
     }
 }
